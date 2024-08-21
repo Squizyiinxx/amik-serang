2384073089975e73c32ea0b3f63137e7e219d6bb
Squizyiinxx <99987609+Squizyiinxx@users.noreply.github.com>
2024-08-22 06:04:14 +0700

    Update fetchUser.ts

--- src/utils/fetchUser.ts
+++ src/utils/fetchUser.ts
@@ -1,6 +1,6 @@
 export const fetchNewsById = async (id: string) => {
     try {
-        const res = await fetch(`NEXT_PUBLIC_API_BASE_URL/api/news/${id}`, { cache: 'no-store' });
+        const res = await fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/news/${id}`, { cache: 'no-store' });
         const data = await res.json();
         return data
     } catch (err) {
@@ -9,7 +9,7 @@ export const fetchNewsById = async (id: string) => {
 }
 
 export const fetchNewsByLimit = async() => {
-    const res = await fetch('NEXT_PUBLIC_API_BASE_URL/api/getNews?limit=3', { cache: 'no-store' });
+    const res = await fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/getNews?limit=3`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch news');
     }
@@ -17,21 +17,21 @@ export const fetchNewsByLimit = async() => {
 }
 
 export const fetchNewsAll = async() => {
-    const res = await fetch('NEXT_PUBLIC_API_BASE_URL/api/getNews', { cache: 'no-store' });
+    const res = await fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/getNews`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch news');
     }
     return res.json();
 }
 export const fetchNewsByCategory = async(id: string) => {
-    const res = await fetch(`NEXT_PUBLIC_API_BASE_URL/api/getNews?category=${id}`, { cache: 'no-store' });
+    const res = await fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/getNews?category=${id}`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch news');
     }
     return res.json();
 }
 export const fetchCategory = async() => {
-    const res = await fetch('NEXT_PUBLIC_API_BASE_URL/api/categories', { cache: 'no-store' });
+    const res = await fetch(`${NEXT_PUBLIC_API_BASE_URL}/api/categories`, { cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch category');
     }
