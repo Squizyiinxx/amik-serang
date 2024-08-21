ee3f5ee91ea4815b9b5c138c1369b761dfdc5a0f
Squizyiinxx <99987609+Squizyiinxx@users.noreply.github.com>
2024-08-22 06:27:30 +0700

    Update page.tsx

--- src/app/admin/news/detail/[id]/page.tsx
+++ src/app/admin/news/detail/[id]/page.tsx
@@ -11,7 +11,7 @@ interface detailProps {
 }
 
 const fetchNewsDetail = async (id: string) => {
-  const res = await fetch(`http://localhost:3000/api/news/${id}`, { cache: 'no-store' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/news/${id}`, { cache: 'no-store' });
   const data = await res.json();
 
   if (data) {
